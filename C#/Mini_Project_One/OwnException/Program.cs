44abeaa792ef6814d89eded3dabd02bee1a5a9d5
unknown <debadyuti.karmakar@gmail.com>
2022-10-21 16:23:16 +0530

    Custom Exception

---
+++ C#/Mini_Project_One/OwnException/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a < 18)
+                {
+                    throw new MyException("You are not elegible");
+                }
+            }
+            catch(MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
+
+
+
+        }
+    }
+
+
+   public class MyException : Exception
+    {
+        public MyException()
+        {
+        }
+
+        public MyException(string msg)
+            : base(msg)
+        {
+        }
+
+        
+    }
+}
