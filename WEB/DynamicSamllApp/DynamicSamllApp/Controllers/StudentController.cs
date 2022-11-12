52968038b3a793167255dbdc581f6f51dd85e1cd
unknown <debadyuti.karmakar@gmail.com>
2022-11-12 21:33:13 +0530

    MVC

---
+++ WEB/DynamicSamllApp/DynamicSamllApp/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicSamllApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Showstudent(String name,int id)
+        {
+            
+            return View();
+        }
+        public IActionResult GetStudent()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEB/DynamicSamllApp/DynamicSamllApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicSamllApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
