7550a52cc874e916cfd198e073193a274487f9db
Debadyuti <debadyuti.karmakar@gmail.com>
2023-01-06 12:19:47 +0530

    Create Regions Controller

---
+++ WEB/WBWalks/WBWalks/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WBWalks.Models.Domain;
+
+namespace WBWalks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>()
+            {
+                new Region
+                {
+                   Id = Guid.NewGuid(),
+                   Name = "Wellington",
+                   Code = "WLG",
+                   Area = 2775,
+                   Lat = -1.8822,
+                   Long = 299.88,
+                   population = 5000000
+
+                 },
+                new Region
+                {
+
+                   Id = Guid.NewGuid(),
+                   Name = "Auckland",
+                   Code = "AUCK",
+                   Area = 27755,
+                   Lat = -1.8822,
+                   Long = 299.88,
+                   population = 5000000
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
